45490ddcb474de0efe2b66c0f8601a66edae0c65
Aizeke <iguido97@icloud.com>
2019-08-25 16:37:12 -0700

    Linked CSS and Added to Logic

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,7 +1,42 @@
-var logic = {
-    createButton: function(){
-        
-    }
-}
-
-module.exports = logic;
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var devoured = $(this).data("newdevoured");
+  
+      var newEatenState = {
+        devoured: devoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed to devoured", devoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $(".txtBox").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
